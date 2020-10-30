da36cbc0d62cddcc10678e15472ee9470eee7db7
reihaneh-kalhor <kalhor.reihaneh@gmail.com>
2020-10-30 10:23:33 +0100

    classes

---
+++ src/main/scala/InputStream.scala
@@ -0,0 +1,26 @@
+
+class InputStream(fileAddress: String) extends java.io.InputStream{
+
+  //A stream is a conceptually endless flow of data
+  //A stream is connected to a data source or a data destination
+  //Streams in Java IO can be either byte based (reading and writing bytes)
+  // or character based (reading and writing characters)
+
+  def open = {
+
+  }
+
+  def readln = {
+
+  }
+
+  def seek(pos: Int): Unit ={
+
+  }
+
+  def endOfStream = {
+
+  }
+
+  override def read(): Int = ???
+}
