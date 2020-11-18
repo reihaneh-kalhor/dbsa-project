82546b913ce7a1d38c3ad1b5cf243e91ecc3382a
Reyhan Kalhor <47897612+reihaneh-kalhor@users.noreply.github.com>
2020-11-18 12:14:33 +0100

    Add files via upload

---
+++ src/main/scala/DBSATest.scala
@@ -0,0 +1,32 @@
+import java.io.FileReader
+
+import org.omg.CORBA.Environment
+
+object DBSATest extends App {
+
+  var inputStream = new InputStream(".\\sampleFile.txt")
+  inputStream.open
+
+  //go forward pointer for 40 position
+  inputStream.seek(40)
+  println(inputStream.toString)
+
+  //read each lines using readline function
+  while(!inputStream.endOfStream(inputStream.fileReader)) {
+    inputStream.readLine
+    println(inputStream.toString)
+  }
+
+  inputStream.resetStringBuffer
+  inputStream.close
+
+  //read each lines using readLineByBuffer function
+  inputStream.open
+
+  while(!inputStream.endOfStream(inputStream.bufferedReader)) {
+    inputStream.readLineByBuffer
+    println(inputStream.toString)
+  }
+
+  inputStream.close
+}
