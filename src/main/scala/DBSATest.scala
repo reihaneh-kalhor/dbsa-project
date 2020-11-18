7716e8c810a3df5293a5e4f2e9c1cfeb4f09d2e8
reihaneh-kalhor <kalhor.reihaneh@gmail.com>
2020-11-18 11:44:25 +0100

    add comments to DBTest file

--- src/main/scala/DBSATest.scala
+++ src/main/scala/DBSATest.scala
@@ -6,18 +6,21 @@ object DBSATest extends App {
 
   var inputStream = new InputStream(".\\sampleFile.txt")
   inputStream.open
-
+  
+  //go forward pointer for 40 position
   inputStream.seek(40)
   println(inputStream.toString)
 
+  //read each lines using readline function
   while(!inputStream.endOfStream(inputStream.fileReader)) {
     inputStream.readLine
     println(inputStream.toString)
   }
 
   inputStream.resetStringBuffer
-
   inputStream.close
+  
+  //read each lines using readLineByBuffer function
   inputStream.open
 
   while(!inputStream.endOfStream(inputStream.bufferedReader)) {
